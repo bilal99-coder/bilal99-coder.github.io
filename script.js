898501d7825f6e06a1bf169918b7a70de091eff9
Bilal Sahli <Bilalsahli1999@gmail.com>
2021-09-22 14:58:20 +0200

    A finished  local  version to be shipped to git. I fixed some bugs related to responsivity of the website with the mobil devices.

--- script.js
+++ script.js
@@ -30,13 +30,13 @@ $(document).ready(function(){
 
 //typing animation script 
 var typed = new Typed(".typing", {
-    strings : ["Youtuber", 'Software engineer', 'Photografer'],
+    strings : ["Youtuber", 'Data ingeniør', 'Fotograf'],
     typeSpeed: 100, 
     backspeed: 60, 
     loop: true
 })
 var typed = new Typed(".typing-2", {
-    strings : ["Youtuber", 'Software engineer', 'Photografer'],
+    strings : ["Youtuber", 'Data ingeniør', 'Fotograf'],
     typeSpeed: 100, 
     backspeed: 60, 
     loop: true
